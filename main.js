57306709c35454890d94e0baac45fc282bb265e1
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-03 21:52:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//Display the word hello three times
+
+for (let i = 0; i <= 3; i++) {
+    console.log("Hello");
+}
+
+
+
+for (let j = 5; j < 20; j+=2) {
+    console.log(j);
+}
+
+for (let a = 10; a > 0; a-- ) {
+    console.log("Happy New Year!!🎆🎉✨🎊");
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    
+    if (i == 5) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+
+for (let a = 10; a > 0; a-- ) {
+    
+    if (a == 7) {
+        break;
+    } else {
+        console.log(a);
+    }
+}
